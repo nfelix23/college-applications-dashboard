88d2e377193ffc38be3ed86f3b4c923de6441134
Miguel Antonio H. Germar <migs.germar@gmail.com>
2022-04-30 20:46:20 +0800

    Make breakdown page empty

--- app_breakdown.py
+++ app_breakdown.py
@@ -4,23 +4,4 @@ import streamlit as st
 import altair as alt
 
 def feature_breakdown(db):
-    st.markdown("This page is under construction. Do not mind the contents.")
-
-    st.write(db.main.head())
-
-    bool_cols = db.ddict.loc[
-        db.ddict["info_type"].isin(["interests", "characteristics"]),
-        "var_name"
-    ].tolist()
-
-    college_stats = (
-        db.main
-        .pivot_table(
-            index = ["name", "location", "college_type"],
-            values = bool_cols,
-            aggfunc = "sum",
-        )
-        .reset_index(drop = False)
-    )
-
-    st.write(college_stats)
+    st.markdown("This page is under construction.")
