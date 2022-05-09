10ebac68c000ed5388922315809113d746cf2d82
Miguel Antonio H. Germar <migs.germar@gmail.com>
2022-05-09 18:09:59 +0800

    Add college info page
    User can select one college, and it shows the charts for that college

---
+++ app_college.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import altair as alt
+
+import app_graph_functions as agf
+
+def feature_college(db):
+
+    # Let user select a college
+    selected_college_index = st.selectbox(
+        "College Name",
+        options = db.colleges.sort_values("name").index.tolist(),
+        format_func = lambda idx: db.colleges.iloc[idx].loc["name"]
+    )
+
+    # Display college's location
+    selected_row = db.colleges.iloc[selected_college_index]
+    name = selected_row["name"]
+    locn = selected_row["location"]
+
+    st.markdown(f"Location: {locn}")
+
+    # Display number of applicants
+    num_applicants = (
+        db.main
+        .loc[db.main["name"] == name, :]
+        .shape[0]
+    )
+    st.markdown(f"Number of respondents who applied: {num_applicants}")
+
+    total_applicants = db.main.drop_duplicates("respondent_code", keep = "first").shape[0]
+    perc_applicants = agf.add_perc_symbol(num_applicants / total_applicants * 100)
+    st.markdown(f"Percentage of respondents who applied: {perc_applicants}")
+
+    # Obtain statistics for that college
+    one_college_stats = db.college_stats.loc[
+        db.college_stats["name"] == name,
+    ]
+
+    # Function to make a chart for a particular info_type
+    def college_info_bar_chart(info_type):
+        """Take an info_type (interests or characteristics) and a representation (students or percentile). Display a bar chart."""
+        col_set = set(
+            db.ddict.loc[
+                db.ddict["info_type"] == info_type,
+                "var_name"
+            ].to_list()
+        )
+
+        mask = one_college_stats["var_name"].isin(col_set)
+
+        filtered_df = one_college_stats.loc[mask]
+
+        filtered_df = filtered_df.merge(
+            right = db.ddict[["var_name", "long_name", "is_other"]]
+        )
+
+        filtered_df["option_type"] = filtered_df["is_other"].apply(
+            lambda x: "Other Option" if x else "Given Option"
+        )
+
+        filtered_df["perc"] = filtered_df["num_students"] / num_applicants * 100
+        filtered_df["perc_str"] = agf.make_perc_col(filtered_df["perc"])
+
+        agf.chart_of_percentages(
+            filtered_df,
+            num_var_title = "Percentage of Applicants",
+            opt_var_title = info_type.title(),
+        )
+
+    # Display a chart for each of these info_type values
+    checkbox_info_types = ["interests", "characteristics"]
+    for info_type in checkbox_info_types:
+        st.markdown(f"## {info_type.title()}")
+        college_info_bar_chart(info_type)
--- app_main.py
+++ app_main.py
@@ -6,6 +6,7 @@ Main script for the College Applications Dashboard.
 import pandas as pd
 import numpy as np
 import streamlit as st
+from scipy import stats
 
 # For connecting to private Google Sheets file
 from google.oauth2 import service_account
@@ -15,6 +16,8 @@ from gsheetsdb import connect
 from app_home import feature_home
 from app_overview import feature_overview
 from app_breakdown import feature_breakdown
+from app_college import feature_college
+import app_graph_functions as agf
 
 if __name__ == "__main__":
 
@@ -103,6 +106,33 @@ if __name__ == "__main__":
             .rename(columns = {"index": "num_apps"})
         )
 
+        # Make new sheet with number of students who chose each option in each college
+        checkbox_info_types = ["interests", "characteristics"]
+        reference_df = db.ddict.loc[
+            db.ddict["info_type"].isin(checkbox_info_types)
+        ]
+        bool_cols = reference_df.loc[:, "var_name"].tolist()
+
+        college_stats = (
+            db.main
+            .pivot_table(
+                index = ["name"],
+                values = bool_cols,
+                aggfunc = "sum",
+            )
+            .reset_index(drop = False)
+        )
+
+        college_stats = pd.melt(
+            college_stats,
+            id_vars = ["name"],
+            value_vars = bool_cols,
+            var_name = "var_name",
+            value_name = "num_students",
+        )
+
+        db["college_stats"] = college_stats
+
         return db
 
     # Obtain data.
@@ -113,6 +143,7 @@ if __name__ == "__main__":
             "page1": "Home Page",
             "page2": "Overview Charts",
             "page3": "Breakdown Charts",
+            "page4": "College Info Charts",
         }
 
         # Radio buttons to select pages of the app
@@ -130,3 +161,6 @@ if __name__ == "__main__":
 
     elif feature == "page3":
         feature_breakdown(db)
+
+    elif feature == "page4":
+        feature_college(db)
